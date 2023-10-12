3e23e00480dc09b0857c55b9725fba0a852cac5d
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-10-12 23:29:15 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:basketball_points_counter_app/BasketballCounterApp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BasketballCounterApp(),
+  ));
+}
